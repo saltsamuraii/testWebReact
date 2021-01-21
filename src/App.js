030c48410d4851aa97807dd0262d2f0f3334c524
Kirill <example@gmail.com>
2021-01-21 17:00:20 +0300

    grid section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Header } from "./components/Header/Header";
-import About from "./components/About/About";
+import { About } from "./components/About/About";
 
 function App() {
     return (
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import s from "./About.module.css";
+import author from '../../assets/author.jpg'
+import DoubleArrow from "../../icons/DoubleArrow.svg";
 
-const About = () => {
+export const About = () => {
     return (
-        <div>
+        <div className={s.about}>
             <nav className={s.nav}>
                 <a className={s.navLink} href="">Home</a>
                 <a className={s.navLink} href="">About Me</a>
                 <a className={s.navLink} href="">Portfolio</a>
                 <a className={s.navLink} href="">Contact</a>
             </nav>
-            <section className={s.content}>
-                {/* <сontainer > */}
-                <div>Photo</div>
-                <h2>About Me</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            <section className={s.about__content}>
+                <img className={s.about__img} src={author} alt="authorPhoto"/>
+                <div>
+                    <h2 className={s.about__title}>About Me</h2>
+                    <p className={s.about__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p className={s.about__text}>Duis aute irure dolor in reprehenderit in
                         voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <div>Jason Wood</div>
-                {/* </сontainer> */}
+                    <div className={s.about__name}>Jason Wood</div>
+                </div>
+                <div className={s.content}>
+                    <h3 className={s.text}>keep scrolling, there is still more to come.</h3>
+                    <img src={DoubleArrow} alt="scroll down"/>
+                </div>
             </section>
         </div>
     );
 };
-
-export default About;
